b43fdfaad02db92ab3c62e32f8e1ad73f6f81e6c
Schell Scivally <efsubenovex@gmail.com>
2009-11-16 18:37:25 +0000

    Netbeans project.

---
+++ pmvcpp/MyTestSuite.h
@@ -0,0 +1,23 @@
+/* 
+ * File:   MyTestSuite.h
+ * Author: schell
+ *
+ * Created on November 16, 2009, 9:59 AM
+ */
+
+#ifndef _MYTESTSUITE_H
+#define	_MYTESTSUITE_H
+
+#include <>
+
+class MyTestSuite {
+public:
+    MyTestSuite();
+    MyTestSuite(const MyTestSuite& orig);
+    virtual ~MyTestSuite();
+private:
+
+};
+
+#endif	/* _MYTESTSUITE_H */
+
---
+++ pmvcpp/build/Debug/GNU-MacOSX/main.o.d
@@ -0,0 +1,3 @@
+build/Debug/GNU-MacOSX/main.o: main.cpp pmvcpp.h
+
+pmvcpp.h:
