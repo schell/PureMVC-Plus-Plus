466e9ed77f3d1b8fd04e947d38817cae33ea10c5
Schell Scivally <efsubenovex@gmail.com>
2009-11-18 00:47:47 +0000

    Added testing project.

---
+++ pmvctest/nbproject/Makefile-variables.mk
@@ -0,0 +1,24 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+CND_BUILDDIR=build
+CND_DISTDIR=dist
+# Debug configuration
+CND_PLATFORM_Debug=GNU-MacOSX
+CND_ARTIFACT_DIR_Debug=dist/Debug/GNU-MacOSX
+CND_ARTIFACT_NAME_Debug=pmvctest
+CND_ARTIFACT_PATH_Debug=dist/Debug/GNU-MacOSX/pmvctest
+CND_PACKAGE_DIR_Debug=dist/Debug/GNU-MacOSX/package
+CND_PACKAGE_NAME_Debug=pmvctest.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/GNU-MacOSX/package/pmvctest.tar
+# Release configuration
+CND_PLATFORM_Release=GNU-MacOSX
+CND_ARTIFACT_DIR_Release=dist/Release/GNU-MacOSX
+CND_ARTIFACT_NAME_Release=pmvctest
+CND_ARTIFACT_PATH_Release=dist/Release/GNU-MacOSX/pmvctest
+CND_PACKAGE_DIR_Release=dist/Release/GNU-MacOSX/package
+CND_PACKAGE_NAME_Release=pmvctest.tar
+CND_PACKAGE_PATH_Release=dist/Release/GNU-MacOSX/package/pmvctest.tar
